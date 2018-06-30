22308d29ae03a277ed342adb248b36c0d265c3ce
Rebecca Lee <rvl26@cornell.edu>
2018-06-30 21:37:11 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,5 +3,14 @@ function paragraphSelector() {
   return $('p');
 }
 function lastImageSelector () {
+  return $('img:last');
+}
+function ninjaBabySelector () {
+  return $('#baby-ninja');
+}
+function divSelector () {
+  return $('.pics');
+}
+function firstListItem () {
   
 }
